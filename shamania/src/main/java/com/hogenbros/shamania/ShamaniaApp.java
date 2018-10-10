506a9f07bb73710f561ab31e4e30dd9515d80a7b
Stephan Hogenboom <stephan.hogenboom@dhl.com>
2018-10-10 17:29:16 +0200

    added a main menu file

---
+++ shamania/src/main/java/com/hogenbros/shamania/ShamaniaApp.java
@@ -0,0 +1,15 @@
+package com.hogenbros.shamania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ShamaniaApp extends SpringBootServletInitializer {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShamaniaApp.class, args);
+    }
+}
---
+++ shamania/src/main/java/com/hogenbros/shamania/controller/MainMenuController.java
@@ -0,0 +1,22 @@
+package com.hogenbros.shamania.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class MainMenuController {
+
+    private Logger LOGGER = LoggerFactory.getLogger(MainMenuController.class);
+
+
+    @RequestMapping("/main")
+    public String getToDoPage(Model model) {
+        LOGGER.info("returning main menu brah");
+        return "main_menu";
+    }
+}
