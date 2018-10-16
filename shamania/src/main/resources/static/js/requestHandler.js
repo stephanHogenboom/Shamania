093cf13a1bef1f26f52585a1c05dab785379b56f
lucashogenboom <hogenboom456@gmail.com>
2018-10-16 23:12:26 +0200

    - changed the ajax template - loading ajax values into client

--- shamania/src/main/resources/static/js/requestHandler.js
+++ shamania/src/main/resources/static/js/requestHandler.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         $.post("/shaman/new/shackleboiii").done(function(){
             $.post("/battle/start/shackleboiii").done(function(value){
                 console.dir(value);
-                //$('#centre-panel').html(value);
+                $('#centre-panel').html(value);
             }).fail(function(){
                 alert("error");
             });
