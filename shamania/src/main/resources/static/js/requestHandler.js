db24d216b36ef3ea6497f51c8e35ba8332682cef
stephanHogenboom <go__for_it@hotmail.com>
2018-10-16 22:56:16 +0200

    edited js

--- shamania/src/main/resources/static/js/requestHandler.js
+++ shamania/src/main/resources/static/js/requestHandler.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#start-button').on('click', function(){
-        $.post("/shaman/new/test").done(function(){
+        $.post("/shaman/new/shackleboiii").done(function(){
             $.post("/battle/start/shackleboiii").done(function(value){
                 console.dir(value);
                 //$('#centre-panel').html(value);
