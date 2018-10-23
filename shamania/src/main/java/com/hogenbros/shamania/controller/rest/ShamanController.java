aa798fe7b47a0ac63d1da7d1274a5dcb8bece856
stephanHogenboom <go__for_it@hotmail.com>
2018-10-23 20:49:48 +0200

    added to the shaman controller

--- shamania/src/main/java/com/hogenbros/shamania/controller/rest/ShamanController.java
+++ shamania/src/main/java/com/hogenbros/shamania/controller/rest/ShamanController.java
@@ -1,20 +1,36 @@
 package com.hogenbros.shamania.controller.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hogenbros.shamania.acces.ShamanRepository;
 import com.hogenbros.shamania.model.Shaman;
+import com.hogenbros.shamania.service.GameMechanics;
+import com.hogenbros.shamania.service.model.Hero;
+import com.hogenbros.shamania.service.model.Role;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Stephan Hogenboom
+ * Simple RestController that is used to create and update shaman entities
+ */
 
 @RestController
 public class ShamanController {
-    Logger logger = LoggerFactory.getLogger(ShamanController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ShamanController.class);
+    private static final int START_LEVEL = 1;
+    private static final int START_STRENGTH = 10;
+    private static final int START_INTELLIGENCE = 10;
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final Role STARTING_ROLE = Role.Conjurer;
 
     @Autowired
     ShamanRepository shamanRepository;
@@ -22,12 +38,24 @@ public class ShamanController {
     @PostMapping("/shaman/new/{fullName}")
     @CrossOrigin
     public void newShaman(@PathVariable String fullName, HttpServletResponse response) {
-        logger.info("shackle");
-        Shaman shaman = new Shaman();
-        shaman.setFullName(fullName);
+        if (fullName == null || fullName.trim().isEmpty()) {
+            throw new IllegalStateException("FullName should not be empty!");
+        }
+        LOG.info("creating new shaman with name= %s", fullName);
+        Shaman shaman = new Shaman(fullName, START_LEVEL, START_STRENGTH, START_INTELLIGENCE);
         shamanRepository.save(shaman);
-        response.setStatus(200);
-        logger.info("shackling done");
+        Hero hero = new Hero(
+                shaman.getFullName(),
+                GameMechanics.caculateMaxHealth(shaman),
+                shaman.getStrenght(),
+                shaman.getIntelligence(),
+                STARTING_ROLE);
+        try {
+            String heroAsJson = MAPPER.writeValueAsString(hero);
+            response.setHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+            response.getOutputStream().write(heroAsJson.getBytes());
+        } catch (IOException e) {
+            response.setStatus(500);
+        }
     }
-
 }
--- shamania/src/main/java/com/hogenbros/shamania/service/GameMechanics.java
+++ shamania/src/main/java/com/hogenbros/shamania/service/GameMechanics.java
@@ -1,5 +1,11 @@
 package com.hogenbros.shamania.service;
 
+import com.hogenbros.shamania.model.Shaman;
+
 public class GameMechanics {
+
+    public static int caculateMaxHealth(Shaman shaman) {
+        return  Math.abs(100 + 10 * shaman.getLevel() + shaman.getStrenght() * 25);
+    }
 
 }
